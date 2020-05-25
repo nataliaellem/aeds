577df623a66cf6dadb4ae88f313f5ddb574ddc8d
Natália Ellem <nataliaem212@gmail.com>
2020-05-25 14:37:46 -0300

    Pre order binary tree mount

--- codigos/binary_tree.c
+++ codigos/binary_tree.c
@@ -91,8 +91,6 @@ void insert_postorder(Node *node, int value){
           int aux2 = node->right->data;
           node->right->data = value;
           insert_postorder(node->right, aux2);
-          print_tree_postorder(node->right);
-          printf("\n");
         } else if (value < node->left->data){
           insert_postorder(node->left, value);
         } else if (value > node->left->data && value < node->right->data){
@@ -107,14 +105,81 @@ void insert_postorder(Node *node, int value){
   }
 }
 
+void insert_preorder(Node *node, int value){
+  if (node !=NULL){
+    if (value > node->data){
+      if (node->left == NULL){
+        node->left = new_node(value);
+      }
+      else if (node->left != NULL && node->right == NULL){
+        if (value > node->left->data){
+          node->right = new_node(value);
+        }
+        else if (value < node->left->data){
+          int aux = node->left->data;
+          node->left->data = value;
+          node->right = new_node(aux);
+        }
+      }
+      else if (node->left != NULL && node->right != NULL){
+        if (value < node->left->data){
+          int aux2 = node->left->data;
+          node->left->data = value;
+          insert_preorder(node->left, aux2);
+        }
+        else if (value > node->right->data){
+          insert_preorder(node->right, value);
+        }
+        else if (value > node->left->data && value < node->right->data){
+          int distance_to_left = (unsigned) (value - node->left->data);
+          int distance_to_right = (unsigned) (value - node->right->data);
+          if (distance_to_left <= distance_to_right){
+            insert_preorder(node->left, value);
+          }
+          else if (distance_to_left > distance_to_right){
+            int aux3 = node->right->data;
+            node->right->data = value;
+            insert_preorder(node->right, aux3);
+          }
+        }
+      }
+    }
+    else if (value < node->data){
+      int aux4 = node->data;
+      node->data = value;
+      insert_preorder(node, aux4);
+    }
+  }
+}
+
 void main() {
   Tree tree = new_tree(2);
-  insert_postorder(tree.root, 7);
-  insert_postorder(tree.root, 4);
-  insert_postorder(tree.root, 3);
-  insert_postorder(tree.root, 1);
-  insert_postorder(tree.root, 6);
-  insert_postorder(tree.root, 5);
-  print_tree_postorder(tree.root);
+  insert_preorder(tree.root, 3);
+  insert_preorder(tree.root, 7);
+  insert_preorder(tree.root, 4);
+  insert_preorder(tree.root, 1);
+  insert_preorder(tree.root, 6);
+  insert_preorder(tree.root, 5);
+  print_tree_preorder(tree.root);
+  printf("\n");
+
+  Tree tree2 = new_tree(8);
+  insert_postorder(tree2.root, 9);
+  insert_postorder(tree2.root, 10);
+  insert_postorder(tree2.root, 11);
+  insert_postorder(tree2.root, 12);
+  insert_postorder(tree2.root, 13);
+  insert_postorder(tree2.root, 14);
+  print_tree_postorder(tree2.root);
+  printf("\n");
+
+  Tree tree3 = new_tree(21);
+  insert_inorder(tree3.root, 15);
+  insert_inorder(tree3.root, 17);
+  insert_inorder(tree3.root, 19);
+  insert_inorder(tree3.root, 16);
+  insert_inorder(tree3.root, 20);
+  insert_inorder(tree3.root, 18);
+  print_tree_inorder(tree3.root);
   printf("\n");
 }
