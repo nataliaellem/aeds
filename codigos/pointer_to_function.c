e7fd91fa8c1986c2aebe1d727b56eea00dfa3557
Alan Borges <sr.alan.bispo@gmail.com>
2020-05-27 23:02:28 -0300

    remodeling

--- codigos/pointer_to_function.c
+++ codigos/pointer_to_function.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-void map(int *array, int (*block)(int), int array_length) {
-  for (size_t i = 0; i < array_length; i++) array[i] = (*block)(array[i]);
+typedef struct user {
+  char *name;
+  char *email;
+  char *password;
+} User;
+
+void printf_email(User user){
+  printf("%s\n", user.email);
 }
 
-int double_x(int x) {
-  return x*2;
+void printf_name(User user){
+  printf("%s\n", user.name);
+}
+
+void each_user(User *users, void (*block)(User)) {
+  for (size_t i = 0; i < 3; i++) {
+    (*block)(users[i]);
+  }
 }
 
 int main() {
-  int *array = (int*) malloc(10 * sizeof(int));
-  for (size_t i = 0; i < 10; i++) array[i] = i + 1;
-  for (size_t i = 0; i < 10; i++) printf("%d - ", array[i]);
-  printf("\n");
+  User users[3];
 
-  map(array, &double_x, 10);
+  for (size_t i = 0; i < 3; i++) {
+    users[i].name = (char*) malloc(50 * sizeof(char));
+    users[i].email = (char*) malloc(50 * sizeof(char));
+    users[i].password = (char*) malloc(50 * sizeof(char));
+  }
 
-  for (size_t i = 0; i < 10; i++) printf("%d - ", array[i]);
-  printf("\n");
+  strcpy(users[0].name, "Natalia");
+  strcpy(users[1].name, "Alan");
+  strcpy(users[2].name, "Pedro");
+  strcpy(users[0].email, "natalia@gmail.com");
+  strcpy(users[1].email, "alan@gmail.com");
+  strcpy(users[2].email, "pedro@gmail.com");
+  strcpy(users[0].password, "123");
+  strcpy(users[1].password, "123");
+  strcpy(users[2].password, "123");
+
+  each_user(users, printf_email);
+  each_user(users, printf_name);
   return 0;
 }
