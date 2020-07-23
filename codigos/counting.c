e61492847c09d1c7b5a09838d0d460fcfb340095
Natália Ellem <nataliaem212@gmail.com>
2020-07-23 17:53:42 -0300

    .

--- codigos/counting.c
+++ codigos/counting.c
@@ -26,5 +26,27 @@ int main() {
   for (int i = 0; i < 5; i++){
     printf("%d\n", new_array[i]);
   }
+
+
+/////////COUNTING WITH THE FILES////////////////////
+int max_id = 5;
+typedef struct user{
+  struct user *next;
+  struct user *prev;
+  char data1;
+  char data2;
+} User;
+
+User *users = (User*) calloc(max_id, sizeof(User));
+FILE *file = fopen("addresses.csv", "r");
+while (i < 5){
+  int id;
+  char scol[4], tcol[4];
+  fscanf(file, "%d,%[^,],%[,],\n", &id, scol, tcol);
+  strcpy(users[id]->data1, scol);
+  strcpy(users[id]->data2, tcol);
+  i++;
+}
+
   return 0;
 }
