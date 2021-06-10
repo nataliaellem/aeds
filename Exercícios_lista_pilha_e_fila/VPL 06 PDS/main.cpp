a37f9396edfbd607868a323b22b95bf3e09bb734
nataliaellem <nataliaem212@gmail.com>
2021-06-10 20:19:41 -0300

    VPL 06 PDS

---
+++ Exercícios_lista_pilha_e_fila/VPL 06 PDS/main.cpp
@@ -0,0 +1,21 @@
+
+// NÃO ALTERE ESSA LINHA
+#include "avaliacao_basica_bovespa.hpp"
+
+int main() {
+  
+    // NÃO ALTERE ESSA LINHA
+    avaliacao_basica();
+
+
+    //
+    // Adicione seu código aqui e faça as demais alterações necessárias
+    //
+
+
+    //
+    // Ao final faça uma chamada para o método 'imprimir_estatisticas_completas()'
+    //
+
+    return 0;
+}
