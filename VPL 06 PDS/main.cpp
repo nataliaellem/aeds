c708d5e45b23b0ef5c615b6f612ac8fba82e43e6
nataliaellem <nataliaem212@gmail.com>
2021-06-11 14:54:55 -0300

    changes to the main file

--- VPL 06 PDS/main.cpp
+++ VPL 06 PDS/main.cpp
@@ -2,20 +2,31 @@
 // NÃO ALTERE ESSA LINHA
 #include "avaliacao_basica_bovespa.hpp"
 
+using namespace std;
+
 int main() {
   
     // NÃO ALTERE ESSA LINHA
     avaliacao_basica();
 
 
-    //
-    // Adicione seu código aqui e faça as demais alterações necessárias
-    //
+    
+    HistoricoBovespa historico;
+    for (int i = 0; i < 12; i++){
+
+        string sigla, data;
+        double valor;
+
+        cin >> sigla;
+        cin >> data;
+        cin >> valor; 
+
+        historico.inserir_cotacao(sigla, data, valor);
+    }
 
 
-    //
-    // Ao final faça uma chamada para o método 'imprimir_estatisticas_completas()'
-    //
+    historico.imprimir_estatisticas_completas();
+    
 
     return 0;
 }
