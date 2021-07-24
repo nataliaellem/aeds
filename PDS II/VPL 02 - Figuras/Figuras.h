e81f9f97d791f190e4fa4e08e4860f34f75530be
nataliaellem <nataliaem212@gmail.com>
2021-07-24 13:59:17 -0300

    alguns testes

--- PDS II/VPL 02 - Figuras/Figuras.h
+++ PDS II/VPL 02 - Figuras/Figuras.h
@@ -1,52 +1,31 @@
 #ifndef FIGURAS_H
 #define FIGURAS_H
 
-/**
- * \class Figure
- *
- * \brief Esta eh a interface de qualquer figura desta aplicacao. O que
- * caracteriza uma figura eh a habilidade de decidir se ela contem ou nao um
- * ponto. Note que figuras e pontos sao representados sempre em duas dimensoes.
- */
+#include <iostream>
+#include <cmath>
+#include <vector>
+
 class Figure {
-  public:
-    /**
-     * \brief Este metodo determina se o ponto (x, y) esta contido na figura.
-     * \param x Coordenada X do ponto.
-     * \param y Coordenada Y do ponto.
-     */
+  public:   
     virtual bool contains(double x, double y) const = 0;
-    /**
-     * \brief A classe prove um destrutor virtual, pois ela foi feita para ser
-     * implementada por outras classes, via heranca.
-     */
     virtual ~Figure() {}
 };
 
-/**
- * \class Rectangle
- *
- * \brief Um retangulo eh uma figura unicamente determinada por um canto
- * inferior esquerdo, mais largura e altura.
- */
+
 class Rectangle: public Figure {
   public:
-    /**
-     * \brief Metodo construtor que cria um retangulo com canto inferior
-     * esquerdo na coordenada (xx, yy), largura igual a ll e altura igual ao
-     * parametro aa.
-     * \param xx Canto esquerdo do retangulo
-     * \param yy Canto inferior do relangulo
-     * \param ll A largura do retangulo
-     * \param aa A altura do retangulo
-     */
     Rectangle(double xx, double yy, double ll, double aa):
       _x(xx), _y(yy), _l(ll), _a(aa) {}
-    /**
-     * \brief Determina se o ponto (x, y) estah contido dentro deste retangulo.
-     */
+
     bool contains(double x, double y) const {
-      // TODO: este metodo deve ser implementado
+
+      if (x >= this->_x && y >= this->_y){
+        double x_largura = this->_x + this->_l;
+        double y_altura = this->_y + this->_a;
+        if (x <= x_largura && y <= y_altura){
+          return true;
+        }
+      }
       return false;
     }
     const double _x; ///< Coordenada X do lado inferior-esquerdo do retangulo
@@ -56,26 +35,30 @@ class Rectangle: public Figure {
 };
 
 
-/**
- * \class Circle
- *
- * \brief Um circulo eh uma figura determinada por um centro e um raio.
- */
+
 class Circle: public Figure {
   public:
-    /**
-     * \brief Metodo contrutor que produz um circulo com centro no ponto
-     * (xx, yy) e raio rr.
-     * \param xx Coordenada X do centro do circulo
-     * \param yy Coordenada Y do centro do circulo
-     * \param rr Raio do circulo
-     */
+  
     Circle(double xx, double yy, double rr): _x(xx), _y(yy), _r(rr) {}
-    /**
-     * \brief Determina se o ponto (x, y) estah contido dentro deste circulo.
-     */
+   
     bool contains(double x, double y) const {
-      // TODO: este metodo deve ser implementado
+      double distancia_x, distancia_y;
+      if (this->_x >= x){
+        distancia_x = this->_x - x;
+      }
+      else if (this->_x < x){
+        distancia_x = x - this->_x;
+      }
+      if (this->_y >= y){
+        distancia_y = this->_y - y;
+      }
+      else if (this->_y < y){
+        distancia_y = y - this->_y;
+      }
+      double distancia = sqrt((distancia_x * distancia_x) + (distancia_y * distancia_y));
+      if (distancia <= this->_r){
+        return true;
+      }
       return false;
     }
     const double _x; ///< Coordenada X do centro do circulo
@@ -83,30 +66,49 @@ class Circle: public Figure {
     const double _r; ///< O raio do circulo
 };
 
-/**
- * \brief Cria uma nova figura formada pela uniao de lhs e rhs. Esta nova
- * figura F deve ter a seguinte invariante: F.contains(px, py) eh verdade se
- * o ponto (px, py) estiver contido em lhs ou em rhs.
- */
+class CompositeFigure: public Figure {
+  public:
+
+    const Figure *_f1;
+    const Figure *_f2;
+    bool _op;
+
+    CompositeFigure(const Figure& f1, const Figure& f2, bool op){
+      this->_f1 = &f1;
+      this->_f2 = &f2;
+      this->_op = op;
+    }
+
+    bool contains(double x, double y) const {
+      if (this->_op == true) { // Significa que as figuras vão ser somadas
+        bool contains_f1 = this->_f1->contains(x, y);
+        bool contains_f2 = this->_f2->contains(x, y);
+        return (contains_f1 || contains_f2);
+      }
+      else { // Significa que é f1 - f2
+        bool contains_f1 = this->_f1->contains(x, y);
+        bool contains_f2 = this->_f2->contains(x, y);
+        return (contains_f1 && !(contains_f2));
+      }
+    }
+
+};
+
+
 Figure* operator+(const Figure& lhs, const Figure& rhs) {
-  // TODO: este metodo deve ser implementado. Lembre-se que voce pode
-  // criar novas classes que estendem Figure, para resolver essa questao.
-  // Uma idea eh criar figuras compostas, que representem adicoes ou
-  // diferencas entre figuras mais basicas.
-  return NULL;
+  
+  CompositeFigure *f = new CompositeFigure(lhs, rhs, true);
+
+
+  return f;
 }
 
-/**
- * \brief Cria uma nova figura formada pela diferenca entre lhs e rhs.
- * Est nova figura, F, possui a seguinte invariante: F.contains(x, y) eh
- * verdade se o ponto (x, y) estiver contido em lhs, mas nao estiver contido
- * em rhs.
- */
+
 Figure* operator-(const Figure& lhs, const Figure& rhs) {
-  // TODO: este metodo deve ser implementado. Novamente, lembre-se que voce
-  // pode criar novas classes que representam figuras. Essas classes
-  // devem implementar a class abstrata Figure.
-  return NULL;
+
+  CompositeFigure *f = new CompositeFigure(lhs, rhs, false);
+  
+  return f;
 }
 
 #endif
