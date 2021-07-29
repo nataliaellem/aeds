aae26b0d545b8484dffa47d8566bd7bd4763c539
nataliaellem <nataliaem212@gmail.com>
2021-07-29 12:30:13 -0300

    operador "==" implementado e testes

--- PDS II/VPL 04 - Polígonos/polygon.cpp
+++ PDS II/VPL 04 - Polígonos/polygon.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <typeinfo>
+#include <cmath>
 #include "polygon.h"
 
 std::ostream& operator << (std::ostream &out, const Polygon &poly) {
@@ -16,17 +17,20 @@ std::ostream& operator << (std::ostream &out, const Polygon &poly) {
 
 bool operator == (const Polygon &lhs, const Polygon &rhs) {
   // TODO: implement this method.
-  if (typeid(rhs) == typeid(RightRectangle)){
-    
-
+  std::vector<Point> limits_lhs = lhs.operator const std::vector<Point, std::allocator<Point>> &();
+  std::vector<Point> limits_rhs = rhs.operator const std::vector<Point, std::allocator<Point>> &();
+  bool resp = false;
+  if (typeid(rhs) == typeid(RightRectangle) || typeid(rhs) == typeid(Point) || typeid(rhs) == typeid(RightSquare)){
+    for (Point p : limits_lhs){
+      resp = rhs.contains(p);
+    }
   }
-  else if (typeid(rhs) == typeid(RightSquare)){
-
+  if (typeid(lhs) == typeid(RightRectangle) || typeid(lhs) == typeid(Point) || typeid(lhs) == typeid(RightSquare)){
+    for (Point p : limits_rhs){
+      resp = lhs.contains(p);
+    }
   }
-  else if (typeid(rhs) == typeid(Point)){
-    
-  }
-  return false;
+  return resp;
 }
 
 bool Point::contains(const Point& p) const {
@@ -40,27 +44,47 @@ std::ostream& operator << (std::ostream &out, const Point &p) {
 
 bool RightRectangle::contains(const Point& p) const {
   // TODO: implement this method.
-  bool resp;
-  if (this->limits[0].x > this->limits[1].x){
-    resp = (p.x >= this->limits[1].x && p.x <= this->limits[0].x);
+
+  Point p1(this->limits[0].x, this->limits[0].y);
+  Point p2(this->limits[1].x, this->limits[1].y);
+  int x3, x4, y3, y4;
+  
+  if (this->limits[0].x < this->limits[1].x){
+    x3 = this->limits[0].x;
+    x4 = this->limits[1].x;
   }
-  else {
-    resp = (p.x >= this->limits[0].x && p.x <= this->limits[1].x);
+  else if (this->limits[1].x < this->limits[0].x){
+    x3 = this->limits[1].x;
+    x4 = this->limits[0].x;
   }
-  if (this->limits[0].y > this->limits[1].y){
-    resp = (p.y >= this->limits[1].y && p.y <= this->limits[0].y);
+  else if (this->limits[1].x == this->limits[0].x){
+    x3 = this->limits[1].x;
+    x4 = this->limits[1].x;
   }
-  else {
-    resp = (p.y >= this->limits[0].y && p.y <= this->limits[1].y);
+
+  if (this->limits[0].y < this->limits[1].y){
+    y4 = this->limits[0].y;
+    y3 = this->limits[1].y;
   }
-  return resp;
+  else if (this->limits[1].y < this->limits[0].y){
+    y4 = this->limits[1].y;
+    y3 = this->limits[0].y;
+  }
+  else if (this->limits[1].y == this->limits[0].y){
+    y4 = this->limits[1].y;
+    y3 = this->limits[1].y;
+  }
+  
+  Point p3(x3, y3);
+  Point p4(x4, y4);
+
+  // p1, p2, p3 e p4 são os quatro pontos que compõem o retângulo
+  return (p.contains(p1) || p.contains(p2) || p.contains(p3) || p.contains(p4));
 }
 
 Point::Point(int xx, int yy): x(xx), y(yy) {
   // TODO: implement this method.
-  
-
-  
+  this->limits.push_back(*this);
 }
 
 RightRectangle::RightRectangle(int x0, int y0, int x1, int y1) {
